826bcbf97f3c72415bf250d218249f155c63c156
Nacho Eigu <ignacio.eiguren@gmail.com>
2022-08-02 09:35:54 -0300

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,17 @@
+MAIN_TOPICS = ['politica', 'sociedad', 'economia', 'internacional', 'policiales', 'tecno','show']
+
+TN_HEADERS = {
+    'authority': 'tn.com.ar',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'accept-language': 'en-US;q=0.8',
+    'cache-control': 'max-age=0',
+    'if-modified-since': 'Thu, 21 Jul 2022 17:39:54 GMT',
+    'if-none-match': 'W/"80f71-z+pLcAQls7KtxLx8wfLDsyOyn6k"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'none',
+    'sec-fetch-user': '?1',
+    'sec-gpc': '1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36',
+}
